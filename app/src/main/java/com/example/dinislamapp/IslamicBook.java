e967d797fea76c4d4b21c4f4f1e213b907420cde
Sifat <tuhid15-13038@diu.edu.bd>
2022-06-20 23:46:40 +0600

    project islam

---
+++ app/src/main/java/com/example/dinislamapp/IslamicBook.java
@@ -0,0 +1,14 @@
+package com.example.dinislamapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IslamicBook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_islamic_book);
+    }
+}
---
+++ app/src/main/java/com/example/dinislamapp/Jiboni.java
@@ -0,0 +1,14 @@
+package com.example.dinislamapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Jiboni extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jiboni);
+    }
+}
